d40043259427c95ca24ff5ac41ecc14d25150b49
Madhuri Matta <mleela.madhuri@strongarmtech.com>
2022-06-13 12:12:42 +0530

    change chromedriver location

--- Tests/features/steps/script.py
+++ Tests/features/steps/script.py
@@ -2,11 +2,12 @@ from behave import *
 from selenium import webdriver
 import time
 import os
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 
 @given('Launch Chrome Browser')
 def LaunchChrome(context):
-    context.driver=webdriver.Chrome(ChromeDriverManager().install() )
+    context.driver=webdriver.Chrome(executable_path="C:\Users\runneradmin\.wdm\drivers\chromedriver\win32\102.0.5005.61")
+    # context.driver=webdriver.Chrome(ChromeDriverManager().install() )
     context.driver.maximize_window()
     time.sleep(5)
 
