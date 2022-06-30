15a30fa18ff3207ec4999bd9a11b5d326bb45471
Madhuri Matta <mleela.madhuri@strongarmtech.com>
2022-06-30 17:41:55 +0530

    trying

--- Tests/features/steps/script.py
+++ Tests/features/steps/script.py
@@ -15,7 +15,7 @@ driver = webdriver.Chrome(chrome_options=chrome_options)
 
 @given('Launch Chrome Browser')
 def LaunchChrome(context):
-    context.driver=webdriver.Chrome(ChromeDriverManager().install() ).get('https://www.google.nl/')
+    context.driver=webdriver.Chrome(ChromeDriverManager().install() ).get('https://www.google.com/')
     context.driver.maximize_window()
     time.sleep(5)
 
