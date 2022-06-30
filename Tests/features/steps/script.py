24dcccc66af0157bce9ad6e51f22852ad4a55c50
Madhuri Matta <mleela.madhuri@strongarmtech.com>
2022-06-30 13:12:18 +0530

    trying

--- Tests/features/steps/script.py
+++ Tests/features/steps/script.py
@@ -12,7 +12,6 @@ chrome_options.add_argument('--window-size=1420,1080')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(chrome_options=chrome_options)
-d.get('https://www.google.nl/')
 
 @given('Launch Chrome Browser')
 def LaunchChrome(context):
