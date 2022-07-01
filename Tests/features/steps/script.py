793913cb1f817b6b6d5a41924aa8243b1d4bf8b4
Madhuri Matta <mleela.madhuri@strongarmtech.com>
2022-07-01 16:38:24 +0530

    trying

--- Tests/features/steps/script.py
+++ Tests/features/steps/script.py
@@ -12,7 +12,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
-self.driver = webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Chrome(chrome_options=chrome_options)
 
 # chrome_options = webdriver.ChromeOptions()
 # chrome_options.add_argument('--no-sandbox')
