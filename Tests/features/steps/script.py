9749c7fdbd32fb9e9b4cf3cdd8959b3f25497c8c
Madhuri Matta <mleela.madhuri@strongarmtech.com>
2022-07-01 16:11:50 +0530

    trying

--- Tests/features/steps/script.py
+++ Tests/features/steps/script.py
@@ -3,15 +3,28 @@ from selenium import webdriver
 import time
 import os
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+options = Options()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+d = webdriver.Chrome(executable_path=r'/home/PycharmProjects/chromedriver', chrome_options=options)
+d.get('https://www.google.nl/')
 
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--window-size=1420,1080')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(chrome_options=chrome_options)
+# from selenium.webdriver.chrome.options import Options
+
+
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--window-size=1420,1080')
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--disable-gpu')
+# driver = webdriver.Chrome(chrome_options=chrome_options)
+
+# capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+#   chromeOptions: { args: %w(headless --no-sandbox disable-gpu window-size=1920,1080) }
+# )
 
 @given('Launch Chrome Browser')
 def LaunchChrome(context):
