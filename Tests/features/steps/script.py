b28d5773ee106359a1c33cd8123d46cb648896a2
Madhuri Matta <mleela.madhuri@strongarmtech.com>
2022-07-04 14:53:51 +0530

    trying with push

--- Tests/features/steps/script.py
+++ Tests/features/steps/script.py
@@ -9,11 +9,11 @@ from pyvirtualdisplay import Display
 display = Display(visible=0, size=(1024, 768))
 display.start()
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(chrome_options=chrome_options)
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--disable-gpu')
+# driver = webdriver.Chrome(chrome_options=chrome_options)
 
 
 @given('Launch Chrome Browser')
